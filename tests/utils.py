2885173bcac3f727d2ce7d25b02bf1d940890df7
pscott <scott.piriou@ledger.fr>
2021-01-29 22:51:56 +0100

    Add comments about test fn

--- tests/utils.py
+++ tests/utils.py
@@ -13,17 +13,24 @@ def close_file(filename):
     except:
         print("could not remove file {}.".format([filename]))
 
+'''
+Checks that stdout and stderr are the equal to those passed in as parameters.
+If the one passed in as a parameter is `None`, then the check is not done.
+'''
 def test(errno, stdout_expected, stderr_expected, filename):
     try:
         output = subprocess.check_output(["./nm", filename], timeout=3, stderr=subprocess.PIPE).decode("utf-8")
-        assert(stdout_expected == output), "Expected: success stdout `{}`, got: `{}`".format(stdout_expected, output)
+        if (stdout_expected):
+            assert(stdout_expected == output), "Expected: success stdout `{}`, got: `{}`".format(stdout_expected, output)
     except subprocess.CalledProcessError as exc:
         returncode = exc.returncode
         assert(errno == exc.returncode), "Expected errno `{}`, got: `{}`".format(errno, returncode)
-        stdout = exc.output.decode(sys.getfilesystemencoding())
-        assert(stdout_expected == stdout), "Expected stdout `{}`, got: `{}`".format(stdout_expected, stdout)
-        stderr = exc.stderr.decode(sys.getfilesystemencoding())
-        assert(stderr_expected == stderr), "Expected stderr `{}`, got: `{}`".format(stderr_expected, stderr)
+        if (stdout_expected):
+            stdout = exc.output.decode(sys.getfilesystemencoding())
+            assert(stdout_expected == stdout), "Expected stdout `{}`, got: `{}`".format(stdout_expected, stdout)
+        if (stderr_expected):
+            stderr = exc.stderr.decode(sys.getfilesystemencoding())
+            assert(stderr_expected == stderr), "Expected stderr `{}`, got: `{}`".format(stderr_expected, stderr)
     print(colored("OK\t\t" + filename, 'green'))
 
 def test_new_file(data, errno, stdout_expected, stderr_expected, filename):
